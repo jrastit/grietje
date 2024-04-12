1bc4f9cc4a5e47da991efde57bb1dab5e041cdae
jrastit <jerome.rastit@libertysurf.fr>
2024-04-12 22:31:40 +0200

    'fix server'

--- script/create_database.sh
+++ script/create_database.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#if [ "$(whoami)" != "postgres" ]; then
-#        echo "Script must be run as user: postgres"
-#        exit 255
-#fi
+if [ "$(whoami)" != "postgres" ]; then
+       echo "Script must be run as user: postgres"
+       exit 255
+fi
 
 PORT=5434
 
@@ -12,4 +12,4 @@ psql -p $PORT -c "create database grietje"
 psql -p $PORT -c "grant all privileges on database grietje to grietje"
 
 psql -p $PORT -c "ALTER DATABASE grietje OWNER TO grietje;"
-psql -p $PORT -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql -p $PORT -d grietje -c "CREATE EXTENSION IF NOT EXISTS postgis;"
